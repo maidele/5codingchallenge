d22afa151e9035976739373e12f9e83a771beb74
Maidele Archelus <maidele@usf.edu>
2025-02-09 09:05:09 -0500

    Edited task 1

--- cc_5.js
+++ cc_5.js
@@ -1,9 +1,10 @@
 // Task 1-
 let customer = { 
     name: "Nancy Franklin", 
-    age: 22, 
-    email: nancyfranklin.com
+    age: "22", 
+    email: "nancyfranklin@gmail.com"
 };
 
-console.log('Customer Name:, ${customer}');
+console.log("Customer Name:", customer);
 
+// Task 2-
